083727a4465a0469c669fd6115ec188e8a0ab966
pmanlig <patrik@manlig.org>
2024-11-30 17:34:16 +0100

    Forgotten variable

--- src/Solvers/S1.js
+++ src/Solvers/S1.js
@@ -3,7 +3,7 @@ import Solver from './Solvers';
 // import { SearchState, PixelMap } from '../util';
 
 export class S1 extends Solver {
-	solve() {
+	solve(input) {
 		return { solution: `Ingen lösning än` };
 	}
 }
