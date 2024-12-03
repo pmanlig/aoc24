5e9fc90049915865554acb92d76589475db4e577
pmanlig <patrik@manlig.org>
2024-12-03 23:15:38 +0100

    Day 3

--- src/Solvers/S3.js
+++ src/Solvers/S3.js
@@ -4,6 +4,14 @@ import Solver from './Solvers';
 
 export class S3 extends Solver {
 	solve(input) {
-		return { solution: `Ingen lösning än` };
+		function calcSum(input) {
+			return [...input.matchAll(/mul\((\d+),(\d+)\)/g)]
+				.map(i => i.slice(1).map(n => parseInt(n, 10)))
+				.map(i => i[0] * i[1])
+				.reduce((a, b) => a + b);
+		}
+		let a = calcSum(input);
+		let b = calcSum(input.split("do()").map(p => p.split("don't()")[0]).join(""));
+		return { solution: `Instruction sum: ${a}\nRefined instruction sum: ${b}` };
 	}
 }
