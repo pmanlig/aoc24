2448496e26358dc87b597d6f58c923712830e0f1
pmanlig <patrik@manlig.org>
2024-12-09 05:51:01 +0100

    Day 8

--- src/Solvers/S6.js
+++ src/Solvers/S6.js
@@ -40,23 +40,6 @@ export class S6 extends Solver {
 			}
 		}
 
-		function markPositions(input, init) {
-			let pos = init;
-			while (pos.x > 0 && pos.x < input[0].length - 1 && pos.y > 0 && pos.y < input.length - 1) {
-				input[pos.y][pos.x] = 'X';
-				let d = pos.d;
-				for (let n = newPos(pos, d); input[n.y][n.x] === '#'; n = newPos(pos, d)) {
-					d = (d + 1) % 4;
-				}
-				pos = newPos(pos, d);
-			}
-			input[pos.y][pos.x] = 'X';
-		}
-
-		function dejaVu(pos, history) {
-			return history.some(p => p.x === pos.x && p.y === pos.y && p.d === pos.d);
-		}
-
 		function findPath(input, init) {
 			let pos = init;
 			let path = [pos];
@@ -73,8 +56,8 @@ export class S6 extends Solver {
 			return path;
 		}
 
+		/* Meh; this produces too good of a solution!
 		function isLoop(input, init, obst) {
-			if (init[0].x === obst.x && init[0].y === obst.y) { return false; }
 			let map = makeMap(input);
 			for (let i = 0; i < init.length; i++) {
 				map[init[i].y][init[i].x] = map[init[i].y][init[i].x] | flags[init[i].d];
@@ -92,6 +75,48 @@ export class S6 extends Solver {
 			}
 			return false;
 		}
+		*/
+
+		function isTrueLoop(map, pos) {
+			let turns = 0;
+			let loop = pos;
+			try {
+				while (turns === 0 || pos.x !== loop.x || pos.y !== loop.y) {
+					let d = pos.d;
+					for (let n = newPos(pos, d); map[n.y][n.x] === obstacle; n = newPos(pos, d)) {
+						d = (d + 1) % 4;
+						turns++;
+					}
+					pos = newPos(pos, d);
+				}
+			} catch (e) {
+				console.log("error", pos);
+			}
+			return turns === 4;
+		}
+
+		function isLoop(input, init, obst) {
+			let map = makeMap(input);
+			for (let i = 0; i < init.length; i++) {
+				map[init[i].y][init[i].x] = map[init[i].y][init[i].x] | flags[init[i].d];
+			}
+			map[obst.y][obst.x] = obstacle;
+			let pos = init.pop();
+			while (pos.x > 0 && pos.x < map[0].length - 1 && pos.y > 0 && pos.y < map.length - 1) {
+				let d = pos.d;
+				for (let n = newPos(pos, d); map[n.y][n.x] === obstacle; n = newPos(pos, d)) {
+					d = (d + 1) % 4;
+				}
+				pos = newPos(pos, d);
+				if (map[pos.y][pos.x] & flags[d] > 0) {
+					console.log(pos, obst);
+					return true;
+					return isTrueLoop(map, pos);
+				}
+				map[pos.y][pos.x] = map[pos.y][pos.x] | flags[d];
+			}
+			return false;
+		}
 
 		try {
 			input = "....#.....\n.........#\n..........\n..#.......\n.......#..\n..........\n.#..^.....\n........#.\n#.........\n......#...";
@@ -101,8 +126,14 @@ export class S6 extends Solver {
 			let a = input.map(l => l.filter(e => e === 'X').length).reduce((a, b) => a + b);
 			let b = 0;
 			for (let i = 1; i < path.length; i++) {
-				
+				if (!path.slice(0, i).some(p => p.x === path[i].x && p.y === path[i].y))
+					if (isLoop(input, path.slice(0, i), path[i])) {
+						// console.log(path[i].x, path[i].y);
+						b++;
+					}
 			}
+			// console.log(path[19]);
+			// console.log(isLoop(input, path.slice(0,19), path[19]));
 			return { solution: `Positions visited: ${a}\n# of possible obstacles: ${b}` };
 		} catch (e) {
 			console.log(e);
--- src/Solvers/S8.js
+++ src/Solvers/S8.js
@@ -4,6 +4,92 @@ import Solver from './Solvers';
 
 export class S8 extends Solver {
 	solve(input) {
-		return { solution: `Ingen lösning än` };
+		function findAntennae(input) {
+			let a = {}
+			for (let r = 0; r < input.length; r++) {
+				for (let c = 0; c < input[r].length; c++) {
+					if (input[r][c] !== '.') {
+						let f = input[r][c];
+						if (a[f] === undefined) { a[f] = []; }
+						a[f].push({ x: c, y: r });
+					}
+				}
+			}
+			return a;
+		}
+
+		function calculateAntiNodes(antennae) {
+			let ans = [];
+			for (let i = 0; i < antennae.length - 1; i++) {
+				for (let j = i + 1; j < antennae.length; j++) {
+					let dx = antennae[i].x - antennae[j].x;
+					let dy = antennae[i].y - antennae[j].y;
+					ans.push({ x: antennae[i].x + dx, y: antennae[i].y + dy });
+					ans.push({ x: antennae[j].x - dx, y: antennae[j].y - dy });
+				}
+			}
+			return ans;
+		}
+
+		function reduce(pt, fac) {
+			while (pt.x % fac === 0 && pt.y % fac === 0) {
+				pt.x /= fac;
+				pt.y /= fac;
+			}
+		}
+
+		function sub(pt, delta) {
+			pt.x -= delta.x;
+			pt.y -= delta.y;
+		}
+
+		function add(pt, delta) {
+			pt.x += delta.x;
+			pt.y += delta.y;
+		}
+
+		function calculateTNodes(antennae, map) {
+			let tns = [];
+			for (let i = 0; i < antennae.length - 1; i++) {
+				for (let j = i + 1; j < antennae.length; j++) {
+					let delta = { x: antennae[i].x - antennae[j].x, y: antennae[i].y - antennae[j].y }
+					const primes = [2, 3, 5, 7, 11, 13];
+					primes.forEach(p => reduce(delta, p));
+					let tn = { x: antennae[i].x, y: antennae[i].y };
+					while (tn.y >= 0 && tn.y < map.length) { sub(tn, delta); }
+					for (add(tn, delta); tn.y >= 0 && tn.y < map.length; add(tn, delta)) { tns.push({ x: tn.x, y: tn.y }); }
+				}
+			}
+			return tns;
+		}
+
+		function within(pt, map) {
+			return pt.y >= 0 && pt.y < map.length && pt.x >= 0 && pt.x < map[pt.y].length;
+		}
+
+		// input = "............\n........0...\n.....0......\n.......0....\n....0.......\n......A.....\n............\n............\n........A...\n.........A..\n............\n............";
+		input = input.split('\n').map(l => l.split(''));
+		let antennae = findAntennae(input);
+		let antinodes = {};
+		for (const [key, value] of Object.entries(antennae)) {
+			calculateAntiNodes(value).forEach(an => {
+				let hash = an.x + an.y * 1000;
+				if (within(an, input) && antinodes[hash] === undefined) {
+					antinodes[hash] = 1;
+				}
+			});
+		}
+
+		let tnodes = {};
+		for (const [key, value] of Object.entries(antennae)) {
+			calculateTNodes(value, input).forEach(tn => {
+				let hash = tn.x + tn.y * 1000;
+				if (within(tn, input) && tnodes[hash] === undefined) {
+					tnodes[hash] = 1;
+				}
+			});
+		}
+
+		return { solution: `# of antinodes: ${Object.keys(antinodes).length}\n# of T-nodes: ${Object.keys(tnodes).length}` };
 	}
 }
